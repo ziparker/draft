f96d42e69defa257f67db9f43e0b53e066f18ffc
ziparker <ziparker@user.noreply.github.com>
2022-02-03 19:15:44 -0500

    wip: work move.

--- xx.cc
+++ xx.cc
@@ -51,22 +51,6 @@ using BufQueue = WaitQueue<BDesc>;
 using BufferPtr = std::shared_ptr<BufferPool::Buffer>;
 using FdMap = std::unordered_map<unsigned, int>;
 
-namespace detail {
-template <class F, class Tuple, std::size_t... I>
-constexpr decltype(auto) apply_impl(F &&f, Tuple &&t, std::index_sequence<I...>)
-{
-    return std::invoke(std::forward<F>(f), std::get<I>(std::forward<Tuple>(t))...);
-}
-}
-
-template <class F, class Tuple>
-constexpr decltype(auto) apply(F &&f, Tuple &&t)
-{
-    return detail::apply_impl(
-        std::forward<F>(f), std::forward<Tuple>(t),
-        std::make_index_sequence<std::tuple_size_v<std::remove_reference_t<Tuple>>>{});
-}
-
 class Reader
 {
 public:
@@ -588,8 +572,8 @@ private:
     {
         while (!q_.done())
         {
-            //if (auto work = q_->get(); work && *work)
-                //(*work)();
+            if (auto work = q_.get())
+                (*work)();
         }
     }
 
